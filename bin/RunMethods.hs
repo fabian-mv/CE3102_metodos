4181e9717832e49c8cafbff2c8c01380ae020287
Fabián Montero <fabian@cluster451.org>
2022-05-25 06:03:43 -0600

    implementa falsa posición

--- bin/RunMethods.hs
+++ bin/RunMethods.hs
@@ -1,16 +1,19 @@
-import ANPI.Ecuaciones_No_Lineales.Biseccion
+{-# LANGUAGE UnicodeSyntax #-}
+
+import ANPI.Ecuaciones_No_Lineales.FalsaPosicion
 import ANPI.Base
 
 main :: IO ()
+
 main = putStrLn . show $ solucion param x_0 where
   
-  x_0 = Biseccion
-    { a = -1
-    , b = 10
+  x_0 = FalsaPosicion
+    { a = 0.5
+    , b = pi/4
     }
 
   param = Param
-    { objetivo  = \x -> x^2 - 3
+    { objetivo  = \x -> cos(x) - x
     , tol       = 10e-8
     , iterMax   = 1000
     }
--- lib/ANPI/Ecuaciones_No_Lineales/FalsaPosicion.hs
+++ lib/ANPI/Ecuaciones_No_Lineales/FalsaPosicion.hs
@@ -1,2 +1,30 @@
-module ANPI.Ecuaciones_No_Lineales.FalsaPosicion
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, MultiParamTypeClasses #-}
+
+module ANPI.Ecuaciones_No_Lineales.FalsaPosicion (FalsaPosicion (..))
 where
+
+import ANPI.Base
+import Numeric.LinearAlgebra
+
+data FalsaPosicion = FalsaPosicion
+  { a :: Double
+  , b :: Double
+  } deriving Show
+
+instance Solucion Criterio FalsaPosicion where
+  error_k criterio aprox = abs . criterio . x_k criterio $ aprox
+  siguiente criterio aprox = FalsaPosicion
+    { a = if bolzano then a aprox else x
+    , b = if bolzano then x       else b aprox
+    } where
+    x       = x_k criterio aprox
+    bolzano = (signum . criterio . a $ aprox) /= signum (criterio x)
+
+x_k criterio aprox = b' - (b' - a') * criterio b' / denom where
+  denom = if   denom' > peps
+          then denom'
+          else error "Denominador se anula."
+ 
+  denom' = criterio b' - criterio a'
+  b'     = b aprox
+  a'     = a aprox
