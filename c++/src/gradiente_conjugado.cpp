9dd725dba1a69dd53511c513c12ab5f2c355c327
Jose Alejandro Chavarria <joalchama@gmail.com>
2022-05-26 16:58:01 -0600

    Implementa gradiente conjugado c++

--- c++/src/gradiente_conjugado.cpp
+++ c++/src/gradiente_conjugado.cpp
@@ -2,7 +2,24 @@
 
 vec gradiente_conjugado(mat A, vec b){
 
-    cout << "hola gradiente \n" <<endl;
+    int n = A.n_rows;
 
+    vec xk = vec(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        
+        vec rk = b - A*xk;
+
+        mat alfa_k = ((rk.t() * rk) / (rk.t() * A * rk));
+
+        xk = xk + (alfa_k(0,0)*rk);
+
+        if (norm(rk) < 1e-6){
+            break;
+        }
+    }
+    
+    return xk;
   
 }
