ee468071d84fbcebaed7014c64bd534a42a7aebc
Alejandro Soto <alejandro@34project.org>
2022-05-29 03:23:48 -0600

    Agrega post-processing a pseudo-inversa

--- lib/ANPI/Sistemas_De_Ecuaciones/Pseudoinversa.hs
+++ lib/ANPI/Sistemas_De_Ecuaciones/Pseudoinversa.hs
@@ -12,18 +12,27 @@ import ANPI.Sistemas_De_Ecuaciones.Base
 import ANPI.Base
 import Text.Read (Lexeme(Ident))
 
-newtype Pseudoinversa = Pseudoinversa
-  { x_k :: Matrix R
+data Pseudoinversa = Pseudoinversa
+  { pseudo_k :: Matrix R
+  {- Por laziness esto no es ineficiente (no se calcula x_k en cada iteración),
+   - ya que solamente se solicita el último x_k, los anteriores se ignoran y
+   - por tanto nunca se computan del todo.
+   -}
+  , x_k      :: Vector R
   } deriving (Show, Generic, NFData)
 
 instance Solucion Sistema Pseudoinversa where
-  error_k sistema aprox = norm_2 (x_kmas1' - x_k') / (denom . norm_2) x_kmas1' where
-    (x_k', x_kmas1') = (x_k aprox, x_k . siguiente sistema $ aprox)
+  error_k sistema aprox = norm_2 (pseudo_kmas1' - pseudo_k') / (denom . norm_2) pseudo_kmas1'
+    where (pseudo_k', pseudo_kmas1') = (pseudo_k aprox, pseudo_k . siguiente sistema $ aprox)
 
-  siguiente sistema aprox = Pseudoinversa
-    { x_k = x_k aprox LA.<> (_2i - a sistema LA.<> x_k aprox)
-    } where _2i = (2 *) . ident . n . a $ sistema
+  siguiente sistema aprox = sol sistema $
+    pseudo_k aprox LA.<> (_2i - a sistema LA.<> pseudo_k aprox)
+    where _2i = (2 *) . ident . n . a $ sistema
 
 x_0 :: Sistema -> Pseudoinversa
-x_0 sistema = Pseudoinversa
-  { x_k = (recip . denom . (^ 2) . norm_2 . a) sistema `scale` tr' (a sistema) }
+x_0 sistema = sol sistema $ (recip . denom . (^ 2) . norm_2 . a) sistema `scale` tr' (a sistema)
+
+sol sistema pseudo_k = Pseudoinversa
+  { pseudo_k = pseudo_k
+  , x_k      = pseudo_k #> b sistema
+  }
